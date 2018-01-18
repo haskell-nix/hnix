93aa53bbc62b7976570e949ec8205fb77d17da6a
Ruben Maher <ruben@maher.fyi>
2018-01-19 09:49:49 +1030

    shell.nix: pin nixpkgs revision to HEAD of nixos-17.09
    This is just for the sake of people not on the stable channel who want to use
    the same version of GHC specified here.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,12 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+{ nixpkgs ? <nixpkgs>
+, system ? builtins.currentSystem
+, compiler ? "ghc801"
+}:
+
+let
+  nixpkgsLocal = import nixpkgs { inherit system; };
+
+  nixpkgsStable = import (nixpkgsLocal.callPackage ./nixpkgs.nix {}) {
+    inherit system;
+  };
+in (import ./default.nix { nixpkgs = nixpkgsStable; inherit compiler; }).env
