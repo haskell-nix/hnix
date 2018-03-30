haskell-nix/hnix

commit 4cedcca9ada3528a4b6e01f114c199fa18944808

Abstract the NScope type within MonadNix

Nix/Eval.hs
@@ -154,7 +154,7 @@ eval (NLet binds e) = do
     traceM "Let..1"
     s <- evalBinds True True binds
     traceM $ "Let..2: s = " ++ show (() <$ s)
-    pushScope (newScope s) e
+    pushScope s e
 
 eval (NIf cond t f) = cond >>= \case
     NVConstant (NBool True) -> t
@@ -162,7 +162,7 @@ eval (NIf cond t f) = cond >>= \case
     _ -> error "condition must be a boolean"
 
 eval (NWith scope e) = scope >>= \case
-    NVSet s -> pushScope (newWeakScope s) e
+    NVSet s -> pushWeakScope s e
     _ -> error "scope must be a set in with statement"
 
 eval (NAssert cond e) = cond >>= \case
@@ -175,7 +175,7 @@ eval (NApp fun arg) = fun >>= \case
         args <- buildArgument params =<< buildThunk arg
         traceM $ "Evaluating function application with args: "
             ++ show (newScope args)
-        clearScopes (pushScope (newScope args) (forceThunk =<< f))
+        clearScopes (pushScope args (forceThunk =<< f))
     NVBuiltin _ f -> f =<< buildThunk arg
     _ -> error "Attempt to call non-function"
 
@@ -246,7 +246,7 @@ buildArgument params arg = case params of
             buildThunk $ clearScopes $ do
                 traceM $ "Evaluating default argument with args: "
                     ++ show (newScope args)
-                pushScopes (extendScope args scope) (forceThunk =<< f)
+                pushScope args $ pushScopes scope $ forceThunk =<< f
         This x | isVariadic -> const (pure x)
                | otherwise  -> error $ "Unexpected parameter: " ++ show k
         These x _ -> const (pure x)
@@ -293,7 +293,7 @@ evalBinds allowDynamic recursive = buildResult . concat <=< mapM go
             mv <- case ms of
                 Nothing -> lookupVar key
                 Just s -> s >>= \case
-                    NVSet s -> pushScope (newScope s) (lookupVar key)
+                    NVSet s -> pushScope s (lookupVar key)
                     x -> error
                         $ "First argument to inherit should be a set, saw: "
                         ++ show (() <$ x)
@@ -310,7 +310,8 @@ evalBinds allowDynamic recursive = buildResult . concat <=< mapM go
             then loebM (encapsulate scope <$> s)
             else traverse (deferInScope scope) s
 
-    encapsulate scope f attrs = deferInScope (extendScope attrs scope) f
+    encapsulate scope f attrs =
+        buildThunk . clearScopes . pushScope attrs . pushScopes scope $ f
 
     insert m (path, value) = attrSetAlter path m value
 

Nix.hs
@@ -8,45 +8,36 @@ import           Nix.Expr (NExpr)
 import           Nix.Lint
 import           Nix.Monad
 import           Nix.Monad.Instance
-import           Nix.Scope
 import           Nix.Utils
 
 -- | Evaluate a nix expression in the default context
 evalTopLevelExpr :: MonadNixEnv m => Maybe FilePath -> NExpr -> m (NValueNF m)
 evalTopLevelExpr mdir expr = do
-    base <- do
-        base <- baseEnv
-        case mdir of
-            Nothing -> return base
-            Just dir -> do
-                ref <- buildThunk $ return $ NVLiteralPath dir
-                let m = Map.singleton "__cwd" ref
-                traceM $ "Setting __cwd = " ++ show dir
-                return $ extendScope m base
-    normalForm =<< pushScopes base (evalExpr expr)
+    base <- baseEnv
+    (normalForm =<<) $ pushScopes base $ case mdir of
+        Nothing -> evalExpr expr
+        Just dir -> do
+            traceM $ "Setting __cwd = " ++ show dir
+            ref <- valueRef $ NVLiteralPath dir
+            pushScope (Map.singleton "__cwd" ref) (evalExpr expr)
 
 evalTopLevelExprIO :: Maybe FilePath -> NExpr -> IO (NValueNF (Cyclic IO))
 evalTopLevelExprIO mdir expr =
-    runReaderT (runCyclic (evalTopLevelExpr mdir expr)) emptyScopes
+    runReaderT (runCyclic (evalTopLevelExpr mdir expr)) []
 
 tracingEvalTopLevelExprIO :: Maybe FilePath -> NExpr
                           -> IO (NValueNF (Cyclic IO))
 tracingEvalTopLevelExprIO mdir expr = do
-    base <- case mdir of
-        Nothing -> run baseEnv emptyScopes
+    traced <- tracingExprEval expr
+    case mdir of
+        Nothing ->
+            runCyclicIO (normalForm =<< (`pushScopes` traced) =<< baseEnv)
         Just dir -> do
-            ref   <- run (buildThunk $ return $ NVLiteralPath dir) emptyScopes
-            let m = Map.singleton "__cwd" ref
             traceM $ "Setting __cwd = " ++ show dir
-            base <- run baseEnv emptyScopes
-            return $ extendScope m base
-    expr' <- tracingExprEval expr
-    thnk  <- run expr' base
-    run (normalForm thnk) base
-  where
-    run = runReaderT . runCyclic
+            ref <- runCyclicIO (valueRef $ NVLiteralPath dir)
+            let m = Map.singleton "__cwd" ref
+            runCyclicIO (baseEnv >>= (`pushScopes` pushScope m traced)
+                                 >>= normalForm)
 
 lintExpr :: NExpr -> IO ()
-lintExpr expr = run (checkExpr expr) =<< run baseEnv emptyScopes
-  where
-    run = runReaderT . runCyclic
+lintExpr expr = runCyclicIO (baseEnv >>= (`pushScopes` checkExpr expr))

Nix/Builtins.hs
@@ -21,13 +21,12 @@ import           GHC.Stack.Types (HasCallStack)
 import           Nix.Atoms
 import           Nix.Monad
 import           Nix.Eval
-import           Nix.Scope
 
-baseEnv :: MonadNixEnv m => m (NestedScopes (NThunk m))
+baseEnv :: MonadNixEnv m => m (NScopes m)
 baseEnv = do
     ref <- buildThunk $ NVSet <$> builtins
     lst <- (("builtins", ref) :) <$> topLevelBuiltins
-    return . NestedScopes . (:[]) . newScope $ Map.fromList lst
+    pushScope (Map.fromList lst) currentScope
   where
     topLevelBuiltins = map mapping . filter isTopLevel <$> builtinsList
 
@@ -83,7 +82,7 @@ extractBool = \case
 apply :: MonadNix m => NThunk m -> NThunk m -> m (NValue m)
 apply f arg = forceThunk f >>= \case
     NVFunction params pred ->
-        (`pushScope` (forceThunk =<< pred)) . newScope
+        (`pushScope` (forceThunk =<< pred))
             =<< buildArgument params arg
     x -> error $ "Trying to call a " ++ show (() <$ x)
 

Nix/Lint.hs
@@ -9,7 +9,6 @@ import           Nix.Atoms
 import           Nix.Eval
 import           Nix.Expr
 import           Nix.Monad
-import           Nix.Scope
 
 nullVal :: MonadNix m => m (NValue m)
 nullVal = return $ NVConstant NNull
@@ -31,7 +30,7 @@ check (NRecSet binds) =
     void $ evalBinds True True (fmap (fmap (const nullVal)) binds)
 
 check (NLet binds e) =
-    (`pushScope` e) . newScope
+    (`pushScope` e)
         =<< evalBinds True True (fmap (fmap (const nullVal)) binds)
 
 -- check (NWith _scope e) = do
@@ -42,15 +41,15 @@ check (NAbs a b) = do
     nv <- buildThunk nullVal
     case a of
         Param name ->
-            pushScope (newScope (Map.singleton name nv)) b
+            pushScope (Map.singleton name nv) b
         ParamSet (FixedParamSet s) Nothing ->
-            pushScope (newScope (nv <$ s)) b
+            pushScope (nv <$ s) b
         ParamSet (FixedParamSet s) (Just m) ->
-            pushScope (newScope (Map.insert m nv (nv <$ s))) b
+            pushScope (Map.insert m nv (nv <$ s)) b
         ParamSet (VariadicParamSet s) Nothing ->
-            pushScope (newScope (nv <$ s)) b
+            pushScope (nv <$ s) b
         ParamSet (VariadicParamSet s) (Just m) ->
-            pushScope (newScope (Map.insert m nv (nv <$ s))) b
+            pushScope (Map.insert m nv (nv <$ s)) b
 
 -- In order to check some of the other operations properly, we'd need static
 -- typing

Nix/Monad.hs
@@ -13,7 +13,6 @@ import           Data.Typeable (Typeable)
 import           GHC.Generics
 import           Nix.Atoms
 import           Nix.Expr.Types
-import           Nix.Scope
 import           Nix.Utils
 
 -- | An 'NValue' is the most reduced form of an 'NExpr' after evaluation
@@ -121,14 +120,15 @@ builtin3 name f =
 -- | A path into the nix store
 newtype StorePath = StorePath { unStorePath :: FilePath }
 
-class MonadFix m => MonadNix m where
-    currentScope :: m (NestedScopes (NThunk m))
-    clearScopes  :: m r -> m r
-    pushScopes   :: NestedScopes (NThunk m) -> m r -> m r
-    lookupVar    :: Text -> m (Maybe (NValue m))
+class (Show (NScopes m), MonadFix m) => MonadNix m where
+    data NScopes m :: *
 
-    pushScope :: Scope (NThunk m) -> m r -> m r
-    pushScope = pushScopes . NestedScopes . (:[])
+    currentScope  :: m (NScopes m)
+    clearScopes   :: m r -> m r
+    pushScope     :: ValueSet m -> m r -> m r
+    pushWeakScope :: ValueSet m -> m r -> m r
+    pushScopes    :: NScopes m  -> m r -> m r
+    lookupVar     :: Text -> m (Maybe (NValue m))
 
     data NThunk m :: *
 
@@ -140,7 +140,7 @@ class MonadFix m => MonadNix m where
     addPath :: FilePath -> m StorePath
 
 deferInScope :: MonadNix m
-             => NestedScopes (NThunk m) -> m (NValue m) -> m (NThunk m)
+             => NScopes m -> m (NValue m) -> m (NThunk m)
 deferInScope scope = buildThunk . clearScopes . pushScopes scope
 
 -- | MonadNixEnv represents all of the effects needed by builtin functions in

Nix/Monad/Instance.hs
@@ -24,7 +24,7 @@ import           System.FilePath
 import           System.Process (readProcessWithExitCode)
 
 newtype Cyclic m a = Cyclic
-    { runCyclic :: ReaderT (NestedScopes (NThunk (Cyclic m))) m a }
+    { runCyclic :: ReaderT [Scope (NThunk (Cyclic m))] m a }
     deriving (Functor, Applicative, Monad, MonadFix, MonadIO)
 
 data Deferred m
@@ -32,18 +32,19 @@ data Deferred m
     -- ^ This is closure over the environment where it was created.
     | ComputedValue (NValue m)
 
-instance MonadNix (Cyclic IO) where
-    -- jww (2018-03-29): We should use actually stacked scopes here, rather
-    -- than constantly merging maps. The number of scope levels will usually
-    -- be manageable, but the number of attributes within scopes can be
-    -- enormous, making this one of the worst implementations.
-    pushScopes s k = Cyclic $ local (combineScopes s) $ do
-        scope <- runCyclic currentScope
-        traceM $ "scope: " ++ show (() <$ scope)
-        runCyclic k
+instance Show (NScopes (Cyclic IO)) where
+    show (NestedScopes xs) = show xs
 
-    clearScopes  = Cyclic . local (const (NestedScopes [])) . runCyclic
-    currentScope = Cyclic ask
+instance MonadNix (Cyclic IO) where
+    newtype NScopes (Cyclic IO) = NestedScopes [Scope (NThunk (Cyclic IO))]
+
+    pushScopes (NestedScopes s) = Cyclic . local (s ++) . runCyclic
+
+    pushScope     s = Cyclic . local (Scope s False:) . runCyclic
+    pushWeakScope s = Cyclic . local (Scope s True:) . runCyclic
+
+    clearScopes  = Cyclic . local (const []) . runCyclic
+    currentScope = Cyclic $ NestedScopes <$> ask
 
     -- If a variable is being asked for, it's needed in head normal form.
     lookupVar k  = Cyclic $ do
@@ -104,8 +105,7 @@ instance MonadNixEnv (Cyclic IO) where
                     -- Use this cookie so that when we evaluate the next
                     -- import, we'll remember which directory its containing
                     -- file was in.
-                    pushScope (newScope (Map.singleton "__cwd" ref))
-                              (evalExpr expr)
+                    pushScope (Map.singleton "__cwd" ref) (evalExpr expr)
         p -> error $ "Unexpected argument to import: " ++ show (() <$ p)
 
     getEnvVar = forceThunk >=> \case
@@ -115,3 +115,6 @@ instance MonadNixEnv (Cyclic IO) where
                 Nothing -> NVStr "" mempty
                 Just v  -> NVStr (Text.pack v) mempty
         p -> error $ "Unexpected argument to getEnv: " ++ show (() <$ p)
+
+runCyclicIO :: Cyclic IO a -> IO a
+runCyclicIO = flip runReaderT [] . runCyclic

Nix/Scope.hs
@@ -20,17 +20,8 @@ newScope m = Scope m False
 newWeakScope :: Map.Map Text a -> Scope a
 newWeakScope m = Scope m True
 
-newtype NestedScopes a = NestedScopes { getNestedScopes :: [Scope a] }
-    deriving Functor
-
-instance Show (NestedScopes a) where
-    show (NestedScopes xs) = show xs
-
-emptyScopes :: NestedScopes a
-emptyScopes = NestedScopes []
-
-scopeLookup :: Text -> NestedScopes a -> Maybe a
-scopeLookup key = para go Nothing . getNestedScopes
+scopeLookup :: Text -> [Scope a] -> Maybe a
+scopeLookup key = para go Nothing
   where
     go (Scope m True)  ms rest =
         -- If the symbol lookup is in a weak scope, first see if there are any
@@ -39,12 +30,6 @@ scopeLookup key = para go Nothing . getNestedScopes
         -- are several weaks scopes in a row, followed by non-weak scopes,
         -- we'll first prefer the symbol from the non-weak scopes, and then
         -- prefer it from the first weak scope that matched.
-        scopeLookup key (NestedScopes (filter (not . scopeWeak) ms))
+        scopeLookup key (filter (not . scopeWeak) ms)
             <|> Map.lookup key m <|> rest
     go (Scope m False) _ rest = Map.lookup key m <|> rest
-
-combineScopes :: NestedScopes a -> NestedScopes a -> NestedScopes a
-combineScopes (NestedScopes xs) (NestedScopes ys) = NestedScopes (xs ++ ys)
-
-extendScope :: Map.Map Text a -> NestedScopes a -> NestedScopes a
-extendScope x (NestedScopes xs) = NestedScopes (newScope x:xs)

commit 94d4b5960c1578cef41281f0402c274f31891173

Fix a scoping bug introduced by 4cedcca9ada3528a4b6e01f114c199fa18944808

Nix/Eval.hs
@@ -246,7 +246,7 @@ buildArgument params arg = case params of
             buildThunk $ clearScopes $ do
                 traceM $ "Evaluating default argument with args: "
                     ++ show (newScope args)
-                pushScope args $ pushScopes scope $ forceThunk =<< f
+                pushScopes scope $ pushScope args $ forceThunk =<< f
         This x | isVariadic -> const (pure x)
                | otherwise  -> error $ "Unexpected parameter: " ++ show k
         These x _ -> const (pure x)
@@ -311,7 +311,7 @@ evalBinds allowDynamic recursive = buildResult . concat <=< mapM go
             else traverse (deferInScope scope) s
 
     encapsulate scope f attrs =
-        buildThunk . clearScopes . pushScope attrs . pushScopes scope $ f
+        buildThunk . clearScopes . pushScopes scope . pushScope attrs $ f
 
     insert m (path, value) = attrSetAlter path m value
 