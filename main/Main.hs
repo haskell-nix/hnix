cab2fe914e9636b2ba8f57f067a241000a151142
John Wiegley <johnw@newartisans.com>
2018-03-30 12:47:49 -0700

    Add a missing import

--- main/Main.hs
+++ main/Main.hs
@@ -7,6 +7,7 @@ import Control.Monad
 import Nix.Builtins
 import Nix.Parser
 import Nix.Pretty
+import Nix.Lint
 import Options.Applicative hiding (ParserResult(..))
 import System.FilePath
 import System.IO
