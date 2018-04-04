debd240d9d0a6469f74291c91e91e0ada837d8fd
John Wiegley <johnw@newartisans.com>
2018-04-04 16:36:15 -0700

    Fix to data presence check

--- tests/Main.hs
+++ tests/Main.hs
@@ -15,7 +15,7 @@ main = do
   langTestsEnv <- lookupEnv "LANGUAGE_TESTS"
   let runLangTests = langTestsEnv == Just "yes"
   when runLangTests $ do
-    exist <- doesDirectoryExist "data/nix"
+    exist <- doesDirectoryExist "data/nix/tests"
     unless exist $
         errorWithoutStackTrace $ unlines
             [ "Directory data/nix does not have any files."
