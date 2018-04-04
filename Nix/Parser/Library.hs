7ddb0919656c29fcd88f9fcfc167d4f4a156bb6e
John Wiegley <johnw@newartisans.com>
2018-04-04 11:47:28 -0700

    Parse single-comments should break at CR
    Fixes #62

--- Nix/Parser/Library.hs
+++ Nix/Parser/Library.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Nix.Parser.Library
   ( module Nix.Parser.Library
   , module X
@@ -10,8 +12,10 @@ module Nix.Parser.Library
 import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.IO.Class
+import           Data.Functor
 import qualified Data.HashSet as HashSet
-import           Data.Text hiding (map)
+import           Data.List (nub)
+import           Data.Text
 import           Text.Parser.Char as X hiding (text)
 import           Text.Parser.Combinators as X
 import           Text.Parser.Expression as X
@@ -35,11 +39,43 @@ newtype NixParser p a = NixParser { runNixParser :: p a }
   deriving (Functor, Applicative, Alternative, Monad, MonadPlus, Parsing, CharParsing, LookAheadParsing, Trifecta.DeltaParsing)
 
 instance TokenParsing p => TokenParsing (NixParser p) where
-  someSpace = NixParser $ buildSomeSpaceParser someSpace commentStyle
+  someSpace = NixParser $ buildSomeSpaceParser' someSpace commentStyle
   nesting = NixParser . nesting . runNixParser
   highlight h = NixParser . highlight h . runNixParser
   semi = token $ char ';' <?> ";"
   token p = p <* whiteSpace
+
+buildSomeSpaceParser' :: forall m. CharParsing m => m () -> CommentStyle -> m ()
+buildSomeSpaceParser' simpleSpace
+    (CommentStyle startStyle endStyle lineStyle nestingStyle)
+  | noLine && noMulti = skipSome (simpleSpace <?> "")
+  | noLine           = skipSome (simpleSpace <|> multiLineComment <?> "")
+  | noMulti          = skipSome (simpleSpace <|> oneLineComment <?> "")
+  | otherwise =
+    skipSome (simpleSpace <|> oneLineComment <|> multiLineComment <?> "")
+  where
+    noLine  = Prelude.null lineStyle
+    noMulti = Prelude.null startStyle
+
+    oneLineComment, multiLineComment, inComment, inCommentMulti :: m ()
+    oneLineComment = try (string lineStyle) *> skipMany (satisfy (\x -> x `notElem` ['\r', '\n']))
+    multiLineComment = try (string startStyle) *> inComment
+    inComment = if nestingStyle then inCommentMulti else inCommentSingle
+    inCommentMulti
+      =   () <$ try (string endStyle)
+      <|> multiLineComment *> inCommentMulti
+      <|> skipSome (noneOf startEnd) *> inCommentMulti
+      <|> oneOf startEnd *> inCommentMulti
+      <?> "end of comment"
+
+    startEnd = nub (endStyle ++ startStyle)
+
+    inCommentSingle :: m ()
+    inCommentSingle
+      =   () <$ try (string endStyle)
+      <|> skipSome (noneOf startEnd) *> inCommentSingle
+      <|> oneOf startEnd *> inCommentSingle
+      <?> "end of comment"
 
 commentStyle :: CommentStyle
 commentStyle = CommentStyle
@@ -102,7 +138,7 @@ someTill :: Alternative f => f a -> f end -> f [a]
 someTill p end = go
   where
     go   = (:) <$> p <*> scan
-    scan = (end *> pure []) <|>  go
+    scan = (end $> []) <|>  go
 
 --------------------------------------------------------------------------------
 parseFromFileEx :: MonadIO m => Parser a -> FilePath -> m (Result a)
