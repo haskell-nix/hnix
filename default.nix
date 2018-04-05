89c314a0515cc2e51513f804ab4356e9c59225d8
John Wiegley <johnw@newartisans.com>
2018-04-05 10:48:04 -0700

    Pin insert-ordered-containers

--- default.nix
+++ default.nix
@@ -18,7 +18,7 @@ let
       , tasty, tasty-hunit, tasty-th, text, transformers, trifecta
       , unordered-containers, these, optparse-applicative, interpolate
       , process, exceptions, bytestring, mtl, monadlist, base16-bytestring
-      , cryptohash, template-haskell, syb, xml
+      , cryptohash, template-haskell, syb, xml, insert-ordered-containers
       }:
       mkDerivation {
         pname = "hnix";
@@ -31,7 +31,7 @@ let
           parsers regex-tdfa regex-tdfa-text semigroups text transformers
           trifecta unordered-containers these process directory filepath
           exceptions bytestring mtl monadlist base16-bytestring cryptohash
-          template-haskell syb xml
+          template-haskell syb xml insert-ordered-containers
         ];
         executableHaskellDepends = [
           ansi-wl-pprint base containers data-fix deepseq optparse-applicative
@@ -53,7 +53,14 @@ let
 
   variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
 
-  drv = variant (haskellPackages.callPackage f {});
+  drv = variant (haskellPackages.callPackage f {
+    insert-ordered-containers = pkgs.fetchFromGitHub {
+      owner = "mightybyte";
+      repo = "insert-ordered-containers";
+      rev = "842b32c012e01ba1930c34c367dab9a9412c332d";
+      sha256 = "182y5ffc68dgdrdkfq7w3zsj8xmig6hdnhv5wm866qcks49i2kn4";
+    };
+  });
 
 in
 
