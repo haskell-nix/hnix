72de2453a9c8b976e87d0d89a3351133c2efbf52
John Wiegley <johnw@newartisans.com>
2018-04-05 16:47:20 -0700

    Satisfy hlint

--- Nix/Builtins.hs
+++ Nix/Builtins.hs
@@ -466,7 +466,7 @@ functionArgs fun = force fun $ \case
         return $ NVSet $ valueThunk . NVConstant . NBool <$>
             case p of
                 Param name -> M.singleton name False
-                ParamSet s _ _ -> isJust <$> (OM.toHashMap s)
+                ParamSet s _ _ -> isJust <$> OM.toHashMap s
     v -> throwError $ "builtins.functionArgs: expected function, got "
             ++ showValue v
 
