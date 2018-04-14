36a59cb8c53f76054d1d84e3f7547f2deaa23800
John Wiegley <johnw@newartisans.com>
2018-04-14 09:47:35 -0700

    Drop some unused code from Nix.Atoms; see Nix.Convert instead

--- src/Nix/Atoms.hs
+++ src/Nix/Atoms.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Nix.Atoms where
@@ -9,7 +8,6 @@ module Nix.Atoms where
 import Codec.Serialise
 import Control.DeepSeq
 import Data.Data
-import Data.HashMap.Strict (HashMap)
 import Data.Text (Text, pack)
 import GHC.Generics
 
@@ -29,33 +27,6 @@ data NAtom
   -- | URIs, which are just string literals, but do not need quotes.
   | NUri Text
   deriving (Eq, Ord, Generic, Typeable, Data, Show, NFData, Serialise)
-
-class ToAtom t where
-  toAtom :: t -> NAtom
-
-instance ToAtom Bool where toAtom = NBool
-instance ToAtom Int where toAtom = NInt . fromIntegral
-instance ToAtom Integer where toAtom = NInt
-
-class FromAtom t where
-  fromAtom :: NAtom -> t
-  fromAtoms :: [NAtom] -> t
-  fromAtomSet :: HashMap Text NAtom -> t
-
--- | Convert a primitive into something which can be made from a
--- constant. So for example `convert 1 :: Expression`
-convert :: (ToAtom prim, FromAtom t) => prim -> t
-convert = fromAtom . toAtom
-
--- | Conversion to environment variables for constants.
-atomToEnvString :: NAtom -> Text
-atomToEnvString = \case
-  NInt i -> pack $ show i
-  NFloat f -> pack $ show f
-  NBool True -> "1"
-  NBool False -> ""
-  NNull -> ""
-  NUri uri -> uri
 
 -- | Translate an atom into its nix representation.
 atomText :: NAtom -> Text
