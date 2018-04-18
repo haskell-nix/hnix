1cb30042df1912e92c535ad836a8857e5a10a203
John Wiegley <johnw@newartisans.com>
2018-04-18 16:09:08 -0700

    Add better tracing in Thunk.hs when <<loop>> is encountered
    This relies on an unsafe global counter, so it's only available when
    ENABLE_TRACING is defined.

--- src/Nix/Thunk.hs
+++ src/Nix/Thunk.hs
@@ -1,4 +1,10 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE CPP #-}
+
+#if ENABLE_TRACING
+{-# LANGUAGE BangPatterns #-}
+#endif
+
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -8,6 +14,16 @@
 module Nix.Thunk where
 
 import {-# SOURCE #-} Nix.Stack
+
+#if ENABLE_TRACING
+import Data.IORef
+import System.IO.Unsafe
+import Nix.Utils
+
+counter :: IORef Int
+counter = unsafePerformIO $ newIORef 0
+{-# NOINLINE counter #-}
+#endif
 
 data Deferred m v
     = Deferred (m v)
@@ -27,27 +43,49 @@ class Monad m => MonadThunk v t m | m -> v, v -> t where
 
 data Thunk m v
     = Value v
-    | Thunk (Var m Bool) (Var m (Deferred m v))
+    | Thunk
+#if ENABLE_TRACING
+          !Int
+#endif
+          (Var m Bool) (Var m (Deferred m v))
 
 valueRef :: v -> Thunk m v
 valueRef = Value
 
 buildThunk :: MonadVar m => m v -> m (Thunk m v)
 buildThunk action =
-    Thunk <$> newVar False <*> newVar (Deferred action)
+#if ENABLE_TRACING
+    let !x = unsafePerformIO (atomicModifyIORef' counter (\x -> (succ x, x))) in
+    Thunk x
+#else
+    Thunk
+#endif
+        <$> newVar False <*> newVar (Deferred action)
 
 forceThunk :: (Framed e m, MonadFile m, MonadVar m)
            => Thunk m v -> (v -> m r) -> m r
 forceThunk (Value ref) k = k ref
+#if ENABLE_TRACING
+forceThunk (Thunk n active ref) k = do
+#else
 forceThunk (Thunk active ref) k = do
+#endif
     eres <- readVar ref
     case eres of
         Computed v -> k v
         Deferred action -> do
             nowActive <- atomicModifyVar active (True,)
             if nowActive
-                then throwError "<<loop>>"
+                then
+#if ENABLE_TRACING
+                    throwError $ "<<loop in #" ++ show n ++ ">>"
+#else
+                    throwError "<<loop>>"
+#endif
                 else do
+#if ENABLE_TRACING
+                    traceM $ "Forcing " ++ show n
+#endif
                     v <- action
                     writeVar ref (Computed v)
                     _ <- atomicModifyVar active (False,)
@@ -56,7 +94,11 @@ forceThunk (Thunk active ref) k = do
 forceEffects :: (Framed e m, MonadFile m, MonadVar m)
              => Thunk m v -> (v -> m r) -> m r
 forceEffects (Value ref) k = k ref
+#if ENABLE_TRACING
+forceEffects (Thunk _ active ref) k = do
+#else
 forceEffects (Thunk active ref) k = do
+#endif
     nowActive <- atomicModifyVar active (True,)
     if nowActive
         then return $ error "forceEffects: a value was expected"
