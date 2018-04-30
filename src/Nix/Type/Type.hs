f5038453ae8000ef05e4e3f5ff027fa23a47a691
John Wiegley <johnw@newartisans.com>
2018-04-30 14:55:25 -0500

    Add all of the Nix types to Nix.Type.Type

--- src/Nix/Type/Type.hs
+++ src/Nix/Type/Type.hs
@@ -6,16 +6,21 @@ newtype TVar = TV String
   deriving (Show, Eq, Ord)
 
 data Type
-  = TVar TVar
-  | TCon String
-  | TArr Type Type
+  = TVar TVar                   -- type variable
+  | TCon String                 -- known type
+  | TArr Type Type              -- type -> type
   deriving (Show, Eq, Ord)
 
-data Scheme = Forall [TVar] Type
+data Scheme = Forall [TVar] Type -- forall a b. a -> b
   deriving (Show, Eq, Ord)
 
-typeInt, typeBool :: Type
-typeInt  = TCon "Int"
-typeBool = TCon "Bool"
+typeInt, typeFloat, typeBool, typePath, typeUri, typeNull :: Type
+typeInt    = TCon "Int"
+typeFloat  = TCon "Float"
+typeBool   = TCon "Bool"
+typeString = TCon "String"
+typePath   = TCon "Path"
+typeUri    = TCon "URI"
+typeNull   = TCon "Null"
 
 type Name = Text
