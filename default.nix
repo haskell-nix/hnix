c8cc271f27707f6d61bbd4c3c163ab5851ccc118
John Wiegley <johnw@newartisans.com>
2018-05-08 16:40:56 -0700

    Work from mightybyte: Minimize deps required, for building with GHCJS

--- default.nix
+++ default.nix
@@ -5,43 +5,49 @@
 , doStrict    ? false
 , rev         ? "255a833e841628c0b834575664eae373e28cdc27"
 , sha256      ? "022xm1pf4fpjjy69g7qz6rpqnwpjcy1l0vj49m8xmgn553cs42ch"
-# , nixpkgs     ? import ((import <nixpkgs> {}).fetchFromGitHub {
-#     owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {
 , nixpkgs     ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
     config.allowUnfree = true;
     config.allowBroken = false;
   }
+# , nixpkgs     ? import ((import <nixpkgs> {}).fetchFromGitHub {
+#     owner = "NixOS"; repo = "nixpkgs"; inherit rev sha256; }) {
 }:
 
 let inherit (nixpkgs) pkgs;
 
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = with pkgs.haskell.lib; self: super: rec {
-      serialise = dontCheck super.serialise;
+    overrides = with pkgs.haskell.lib; self: super:
+      if compiler == "ghcjs" then {} else
+      {
+        cryptohash-md5    = doJailbreak super.cryptohash-md5;
+        cryptohash-sha1   = doJailbreak super.cryptohash-sha1;
+        cryptohash-sha256 = doJailbreak super.cryptohash-sha256;
+        cryptohash-sha512 = doJailbreak super.cryptohash-sha512;
+        serialise         = dontCheck super.serialise;
 
-      compact =
-        if compiler == "ghc842"
-        then doJailbreak super.compact
-        else super.compact;
+        compact =
+          if compiler == "ghc842"
+          then doJailbreak super.compact
+          else super.compact;
 
-      ghc-datasize =
-        if doProfiling
-        then null
-        else pkgs.haskell.lib.overrideCabal super.ghc-datasize (attrs: {
-               enableLibraryProfiling    = false;
-               enableExecutableProfiling = false;
-             });
+        ghc-datasize =
+          if doProfiling
+          then null
+          else pkgs.haskell.lib.overrideCabal super.ghc-datasize (attrs: {
+                 enableLibraryProfiling    = false;
+                 enableExecutableProfiling = false;
+               });
 
-      ghc-heap-view =
-        if doProfiling
-        then null
-        else pkgs.haskell.lib.overrideCabal super.ghc-heap-view (attrs: {
-               enableLibraryProfiling    = false;
-               enableExecutableProfiling = false;
-             });
-    };
+        ghc-heap-view =
+          if doProfiling
+          then null
+          else pkgs.haskell.lib.overrideCabal super.ghc-heap-view (attrs: {
+                 enableLibraryProfiling    = false;
+                 enableExecutableProfiling = false;
+               });
+      };
   };
 
 in haskellPackages.developPackage {
@@ -52,11 +58,8 @@ in haskellPackages.developPackage {
 
   modifier = drv: pkgs.haskell.lib.overrideCabal drv (attrs: {
     testHaskellDepends = attrs.testHaskellDepends ++
-      [
-        pkgs.nix
-        haskellPackages.hpack
-        # haskellPackages.cabal-install
-      ];
+      [ pkgs.nix
+        haskellPackages.hpack ];
 
     enableLibraryProfiling    = doProfiling;
     enableExecutableProfiling = doProfiling;
