44ad700a53642439ac9fc6eec2e1da90cb7f0c64
John Wiegley <johnw@newartisans.com>
2018-05-15 12:16:02 -0700

    Minor reformatting in default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,21 @@
 { compiler    ? "ghc822"
+
 , doProfiling ? false
 , doBenchmark ? false
 , doTracing   ? false
 , doStrict    ? false
-, rev         ? "a2981671523416ad5c9a49de8c16ac8fccb2306b"
-, sha256      ? "1cgp63hdzfs836ynvfsknp43dkgq0dfny1bb79c97rzh1alqx0kz"
-, nixpkgs     ? if builtins.compareVersions builtins.nixVersion "2.0" < 0
-                then abort "hnix requires at least nix 2.0"
-                else import (builtins.fetchTarball {
-                     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-                     inherit sha256; }) {
-                     config.allowUnfree = true;
-                     config.allowBroken = false;
-                     }
+
+, rev     ? "a2981671523416ad5c9a49de8c16ac8fccb2306b"
+, sha256  ? "1cgp63hdzfs836ynvfsknp43dkgq0dfny1bb79c97rzh1alqx0kz"
+, nixpkgs ?
+    if builtins.compareVersions builtins.nixVersion "2.0" < 0
+    then abort "hnix requires at least nix 2.0"
+    else import (builtins.fetchTarball {
+           url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+           inherit sha256; }) {
+           config.allowUnfree = true;
+           config.allowBroken = false;
+         }
 }:
 
 let inherit (nixpkgs) pkgs;
@@ -73,7 +76,7 @@ in haskellPackages.developPackage {
                ghc842 = "2.2.0.0";
              }; in
          haskellPackages.callHackage "cabal-install"
-          (cabalInstallVersion.${compiler}) {})
+          cabalInstallVersion.${compiler} {})
       ];
 
     enableLibraryProfiling    = doProfiling;
