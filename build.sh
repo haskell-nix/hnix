1a8baf46c94fdb2878fe22e6234e117c89b58a91
Domen Kožar <domen@dev.si>
2018-08-06 19:00:58 +0100

    download cachix with smaller closure, pin nixpkgs

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -xe
+#!/usr/bin/env bash
 
+set -xe
 set -euo pipefail
 IFS=$'\n\t'
 
 GHCVERSION=${GHCVERSION:-ghc822}
 STRICT=${STRICT:-false}
 TRACING=${TRACING:-false}
+NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/6820e2f0dd16104961d6fc7e8e38846807159c4e.tar.gz
 
 if [ "$GHCVERSION" = "ghcjs" ]; then
     nix-build --substituters 'https://nixcache.reflex-frp.org?trusted=1' ghcjs
