ed7132120f548d3e149ab5493f751b9bf6c9f716
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-03-09 16:36:37 -0500

    This test should exercise enough cases

--- tests/eval-compare/builtins.lessThan-01.nix
+++ tests/eval-compare/builtins.lessThan-01.nix
@@ -1,27 +1,44 @@
 with builtins;
 
-let a = 4.000000000000000000001;
-    b = 4;
-    c = "abcd";
-    d = "abce";
-    e = -4.1;
-    f = -4;
-    g = 0;
-    h = -0;
-    i = 999999999999999999;     # note: this is max bound
-    j = 999999999999999998;
-    k = -999999999999999999;    # note: this is min bound
-    l = -999999999999999998;
-in [(lessThan a b)
-    (lessThan b a)
-    (lessThan c d)
-    (lessThan d c)
-    (lessThan e f)
-    (lessThan f e)
-    (lessThan g h)
-    (lessThan h g)
-    (lessThan i j)
-    (lessThan j i)
-    (lessThan k l)
-    (lessThan l k)
+let numTestPrecisionA = 4.000000000000000000001;
+    numTestPrecisionB = 4;
+    numTest3 = -4.1;
+    numTest4 = -4;
+    numTestZeroA = 0;
+    numTestZeroB = -0;
+    numTestMaxBoundA = 999999999999999999;
+    numTestMaxBoundB = 999999999999999998;
+    numTestMinBoundA = -999999999999999999;
+    numTestMinBoundB = -999999999999999998;
+    stringTest1 = "abcd";
+    stringTest2 = "abce";
+    stringTestBase1 = "foo" + "/" + stringTest1;
+    stringTestBase2 = "foo" + "/" + stringTest2;
+    stringTestJSONA = toJSON stringTest1;
+    stringTestJSONB = toJSON stringTest2;
+    stringTestToFileA = toFile "stringTest1" stringTest1;
+    stringTestToFileB = toFile "stringTest2" stringTest2;
+in [(lessThan numTestPrecisionA numTestPrecisionB)
+    (lessThan numTestPrecisionB numTestPrecisionA)
+    (lessThan numTest3 numTest4)
+    (lessThan numTest4 numTest3)
+    (lessThan numTestZeroA numTestZeroB)
+    (lessThan numTestZeroB numTestZeroA)
+    (lessThan numTestMaxBoundA numTestMaxBoundB)
+    (lessThan numTestMaxBoundB numTestMaxBoundA)
+    (lessThan numTestMinBoundA numTestMinBoundB)
+    (lessThan numTestMinBoundB numTestMinBoundA)
+    (lessThan stringTest1 stringTest2)
+    (lessThan stringTest2 stringTest1)
+    (lessThan stringTestJSONA stringTestJSONB)
+    (lessThan stringTestJSONB stringTestJSONA)
+    (lessThan stringTest1 stringTestJSONB)
+    (lessThan stringTestJSONB stringTest1)
+    (lessThan stringTest2 stringTestJSONA)
+    (lessThan stringTestJSONA stringTest2)
+    (lessThan stringTest1 stringTestToFileB)
+    (lessThan stringTestToFileB stringTest1)
+    (lessThan stringTestToFileA stringTest2)
+    (lessThan stringTest1 (baseNameOf stringTestBase1))
+    (lessThan stringTest2 (baseNameOf stringTestBase2))
   ]
