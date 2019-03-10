259ae914d76f41470151cb7da300eaac1eccc38b
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2019-03-09 23:22:11 -0500

    Found failing case: "stringed" numbers as key

---
+++ tests/eval-compare/builtins.fromJSON-01.nix
@@ -0,0 +1,64 @@
+with builtins;
+
+let simpleJSON = "{\"foo\": \"39\", \"bar\": 472}";
+    screweyJSON = "{\"4275\": \"Please do not fail.\"}";
+      # crazyJSON should be tested once screweyJSON works!
+    crazyJSON = "     {
+    \"response\": {
+        \"success\": 1,
+        \"current_time\": 1362339098,
+        \"prices\": {
+            \"35\": {
+                \"11\": {
+                    \"0\": {
+                        \"current\": {
+                            \"currency\": \"keys\",
+                            \"value\": 39,
+                            \"value_high\": 41,
+                            \"date\": 1357515306
+                        },
+                        \"previous\": {
+                            \"currency\": \"keys\",
+                            \"value\": 37,
+                            \"value_high\": 39
+                        }
+                    }
+                },
+                \"3\": {
+                    \"0\": {
+                        \"current\": {
+                            \"currency\": \"metal\",
+                            \"value\": 0.33,
+                            \"value_high\": 0.66
+                        }
+                    }
+                }
+            },
+            \"5002\": {
+                \"6\": {
+                    \"0\": {
+                        \"current\": {
+                            \"currency\": \"usd\",
+                            \"value\": 0.39,
+                            \"value_high\": 0.42,
+                            \"date\": 1358090106
+                        }
+                    }
+                }
+            },
+            \"5022\": {
+                \"6\": {
+                    \"1\": {
+                        \"current\": {
+                            \"currency\": \"metal\",
+                            \"value\": 1.33,
+                            \"value_high\": 1.55,
+                            \"date\": 1357515175
+                        }
+                    }
+                }
+            }
+        }
+    }
+}";
+in [(fromJSON simpleJSON) (fromJSON screweyJSON)]
