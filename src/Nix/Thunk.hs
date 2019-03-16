fcddfab5a0597efeb38e733ab5a11c8acebd3621
John Wiegley <johnw@newartisans.com>
2019-03-16 14:49:48 -0700

    Add a documentation note for 'thunkId'

--- src/Nix/Thunk.hs
+++ src/Nix/Thunk.hs
@@ -10,6 +10,9 @@ import Data.Typeable (Typeable)
 
 class Monad m => MonadThunk t m v | t -> m, t -> v where
     thunk :: m v -> m t
+    -- | Return an identifier for the thunk unless it is a pure value (i.e.,
+    --   strictly an encapsulation of some 'v' without any additional
+    --   structure). For pure values represented as thunks, returns Nothing.
     thunkId :: t -> Maybe Int
     query :: t -> r -> (v -> r) -> r
     queryM :: t -> m r -> (v -> m r) -> m r
