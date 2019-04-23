a900ea9d6b0c74fae1b0fb9179503f4b68838383
Anton-Latukha <anton.latukha@gmail.com>
2019-04-23 16:28:05 +0300

    Effects: Basic: LANGUAGE: rm unused MultiParamTypeClasses

--- src/Nix/Effects/Basic.hs
+++ src/Nix/Effects/Basic.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
