39ab999801912fa1887da6fa4bb7e8d4df23dce4
Anton-Latukha <anton.latukha@gmail.com>
2020-05-27 17:43:47 +0300

    CI: Travis: build.sh: rm: no longer usable and even harmful vars
    M  build.sh

--- build.sh
+++ build.sh
@@ -6,10 +6,6 @@ set -xe
 set -euo pipefail
 IFS=$'\n\t'
 
-STRICT=${STRICT:-false}
-TRACING=${TRACING:-false}
-NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/6820e2f0dd16104961d6fc7e8e38846807159c4e.tar.gz
-
 if [ "$GHCVERSION" = "ghcjs" ]; then
     nix-build --substituters 'https://nixcache.reflex-frp.org?trusted=1' ghcjs
 else
