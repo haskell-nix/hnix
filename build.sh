58868021a92ce93ec5a7e269f50c67de5a7386b1
Anton-Latukha <anton.latukha@gmail.com>
2020-05-27 17:44:49 +0300

    CI: Travis: build.sh: add: vars checks and defaults
    M  build.sh

--- build.sh
+++ build.sh
@@ -14,5 +14,30 @@ else
         --arg doTracing $TRACING                \
         --arg doStrict $STRICT                  \
         $@
-GHCVERSION=${GHCVERSION:-ghc865}
 fi
+# NOTE: If var not imported - set to the default value
+GHCVERSION=${GHCVERSION:-ghc865}
+rev=${rev:-nixpkgs-unstable}
+NIX_PATH=${NIX_PATH:-"nixpkgs=https://github.com/nixos/nixpkgs/archive/$rev.tar.gz"}
+export NIX_PATH
+name=${name:-defaultBinaryName}
+pkgName=${pkgName:-defaultPkgName}
+failOnAllWarnings=${failOnAllWarnings:-'false'}
+checkUnusedPackages=${checkUnusedPackages:-'false'}
+doCoverage=${doCoverage:-'false'}
+doHaddock=${doHaddock:-'false'}
+doJailbreak=${doJailbreak:-'false'}
+doCheck=${doCheck:-'true'}
+doBenchmark=${doBenchmark:-'false'}
+enableExecutableProfiling=${enableExecutableProfiling:-'false'}
+enableLibraryProfiling=${enableLibraryProfiling:-'false'}
+buildFromSdist=${buildFromSdist:-'false'}
+buildStrictly=${buildStrictly:-'false'}
+disableOptimization=${disableOptimization:-'true'}
+buildStackProject=${buildStackProject:-'false'}
+# NOTE: *Oprparse* key is redifined in the code further
+generateOptparseApplicativeCompletion=${generateOptparseApplicativeCompletion:-'false'}
+allowInconsistentDependencies=${allowInconsistentDependencies:-'false'}
+ghcjsTmpLogFile=${ghcjsTmpLogFile:-'/tmp/ghcjsTmpLogFile.jog'}
+ghcjsLogTailLength=${ghcjsLogTailLength:-'10000'}
+
