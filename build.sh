76b6258198c8cb3287594a14a77875cf5e116410
Anton-Latukha <anton.latukha@gmail.com>
2020-07-06 20:16:14 +0300

    build.sh: short-curcuit the Nix tools update
    M  build.sh

--- build.sh
+++ build.sh
@@ -188,7 +188,8 @@ MAIN() {
 
 # Overall it is useful to have in CI test builds the latest stable Nix
 # 2020-06-24: HACK: Do not ask why different commands on Linux and macOS. IDK, wished they we the same. These are the only commands that worked on according platforms right after the fresh Nix installer rollout.
-(nix-channel --update && nix-env -u) || (sudo nix upgrade-nix)
+# 2020-07-06: HACK: GitHub Actions CI shown that nix-channel or nix-upgrade-nix do not work, there is probably some new rollout, shortcircuting for the time bing with || true
+(nix-channel --update && nix-env -u) || (sudo nix upgrade-nix) || true
 
 
 # Report the Nixpkgs channel revision
