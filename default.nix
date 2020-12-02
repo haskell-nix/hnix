92fe5496ee8c215748f7bbfd3d4eed30b482503a
Anton-Latukha <anton.latukha@gmail.com>
2020-12-02 17:13:45 +0200

    default.nix: upd rev

--- default.nix
+++ default.nix
@@ -91,7 +91,7 @@
 #   , nixos-20.03  # Last stable release, gets almost no updates to recipes, gets only required backports
 #   ...
 #   }
-, rev ? "1dc37370c489b610f8b91d7fdd40633163ffbafd"
+, rev ? "24eb3f87fc610f18de7076aee7c5a84ac5591e3e"
 
 , pkgs ?
     if builtins.compareVersions builtins.nixVersion "2.0" < 0
