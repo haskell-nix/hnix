80b283d126cbddc3812d32185cd3823848539d5e
Anton-Latukha <anton.latukha@gmail.com>
2021-01-07 06:19:13 +0200

    benchmarks/ParserBench: clean-up

--- benchmarks/ParserBench.hs
+++ benchmarks/ParserBench.hs
@@ -2,20 +2,20 @@ module ParserBench (benchmarks) where
 
 import           Nix.Parser
 
-import           Control.Applicative
 import           Criterion
 
 benchFile :: FilePath -> Benchmark
-benchFile = bench <*> whnfIO . parseNixFile . ("data/" ++)
+benchFile = bench <*> whnfIO . parseNixFile . ("data/" <>)
 
 benchmarks :: Benchmark
 benchmarks = bgroup
-  "Parser"
-  [ benchFile "nixpkgs-all-packages.nix"
-  , benchFile "nixpkgs-all-packages-pretty.nix"
-  , benchFile "let-comments.nix"
-  , benchFile "let-comments-multiline.nix"
-  , benchFile "let.nix"
-  , benchFile "simple.nix"
-  , benchFile "simple-pretty.nix"
-  ]
+  "Parser" $
+    fmap benchFile
+      [ "nixpkgs-all-packages.nix"
+      , "nixpkgs-all-packages-pretty.nix"
+      , "let-comments.nix"
+      , "let-comments-multiline.nix"
+      , "let.nix"
+      , "simple.nix"
+      , "simple-pretty.nix"
+      ]
