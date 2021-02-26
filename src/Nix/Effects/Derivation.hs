7bcbead511daa1b2b4a6e3259ce1c2e052457711
Anton-Latukha <anton.latukha@gmail.com>
2021-02-26 17:34:31 +0200

    Effects.Derivation: m refactor

--- src/Nix/Effects/Derivation.hs
+++ src/Nix/Effects/Derivation.hs
@@ -7,7 +7,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
- 
+
 
 module Nix.Effects.Derivation ( defaultDerivationStrict ) where
 
@@ -18,6 +18,7 @@ import           Control.Monad                  ( (>=>), forM, when )
 import           Control.Monad.Writer           ( join, lift )
 import           Control.Monad.State            ( MonadState, gets, modify )
 
+import           Data.Bool                      ( bool )
 import           Data.Char                      ( isAscii, isAlphaNum )
 import qualified Data.HashMap.Lazy             as M
 import qualified Data.HashMap.Strict           as MS
@@ -304,13 +305,15 @@ buildDerivationWithContext drvAttrs = do
       hashMode    <- getAttrOr "outputHashMode"    Flat    $ extractNoCtx >=> parseHashMode
       outputs     <- getAttrOr "outputs"           ["out"] $ mapM (fromValue' >=> extractNoCtx)
 
-      mFixedOutput <- case mHash of
-        Nothing -> pure Nothing
-        Just hash -> do
-          when (outputs /= ["out"]) $ lift $ throwError $ ErrorCall $ "Multiple outputs are not supported for fixed-output derivations"
-          hashType <- getAttr "outputHashAlgo" $ extractNoCtx
-          digest <- lift $ either (throwError . ErrorCall) pure $ Store.mkNamedDigest hashType hash
-          pure $ pure digest
+      mFixedOutput <-
+        maybe
+          (pure Nothing)
+          (\ hash -> do
+            when (outputs /= ["out"]) $ lift $ throwError $ ErrorCall $ "Multiple outputs are not supported for fixed-output derivations"
+            hashType <- getAttr "outputHashAlgo" extractNoCtx
+            digest <- lift $ either (throwError . ErrorCall) pure $ Store.mkNamedDigest hashType hash
+            pure $ pure digest)
+          mHash
 
       -- filter out null values if needed.
       attrs <- if not ignoreNulls
@@ -332,10 +335,11 @@ buildDerivationWithContext drvAttrs = do
 
       pure $ defaultDerivation { platform, builder, args, env,  hashMode, useJson
         , name = drvName
-        , outputs = Map.fromList $ fmap (\o -> (o, "")) outputs
+        , outputs = Map.fromList $ fmap (, mempty) outputs
         , mFixed = mFixedOutput
         }
   where
+
     -- common functions, lifted to WithStringContextT
 
     demand' :: NValue t f m -> (NValue t f m -> WithStringContextT m a) -> WithStringContextT m a
@@ -373,9 +377,11 @@ buildDerivationWithContext drvAttrs = do
       pure name
 
     extractNoCtx :: MonadNix e t f m => NixString -> WithStringContextT m Text
-    extractNoCtx ns = case getStringNoContext ns of
-      Nothing -> lift $ throwError $ ErrorCall $ "The string " <> show ns <> " is not allowed to have a context."
-      Just v -> pure v
+    extractNoCtx ns =
+      maybe
+        (lift $ throwError $ ErrorCall $ "The string " <> show ns <> " is not allowed to have a context.")
+        pure
+        (getStringNoContext ns)
 
     assertNonNull :: MonadNix e t f m => Text -> WithStringContextT m Text
     assertNonNull t = do
