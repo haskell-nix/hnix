e51b07759d4243e46d75c811f12411f92475b0ce
Anton-Latukha <anton.latukha@gmail.com>
2021-03-29 13:22:38 +0300

    Thunk: layout

--- src/Nix/Thunk.hs
+++ src/Nix/Thunk.hs
@@ -9,34 +9,65 @@ import           Control.Monad.Trans.Writer ( WriterT )
 import qualified Text.Show
 
 
-class ( Monad m
-      , Eq (ThunkId m)
-      , Ord (ThunkId m)
-      , Show (ThunkId m)
-      , Typeable (ThunkId m)
-      )
-      => MonadThunkId m where
+-- ** @class MonadThunkId@ & @instances@
+
+class
+  ( Monad m
+  , Eq (ThunkId m)
+  , Ord (ThunkId m)
+  , Show (ThunkId m)
+  , Typeable (ThunkId m)
+  )
+  => MonadThunkId m
+ where
   type ThunkId m :: *
+
   freshId :: m (ThunkId m)
   default freshId
-      :: ( MonadThunkId m'
-        , MonadTrans t
-        , m ~ t m'
-        , ThunkId m ~ ThunkId m'
-        )
-      => m (ThunkId m)
+    :: ( MonadThunkId m'
+      , MonadTrans t
+      , m ~ t m'
+      , ThunkId m ~ ThunkId m'
+      )
+    => m (ThunkId m)
   freshId = lift freshId
 
-instance MonadThunkId m => MonadThunkId (ReaderT r m) where
+
+-- *** Instances
+
+instance
+  MonadThunkId m
+  => MonadThunkId (ReaderT r m)
+ where
   type ThunkId (ReaderT r m) = ThunkId m
-instance (Monoid w, MonadThunkId m) => MonadThunkId (WriterT w m) where
+
+instance
+  ( Monoid w
+  , MonadThunkId m
+  )
+  => MonadThunkId (WriterT w m)
+ where
   type ThunkId (WriterT w m) = ThunkId m
-instance MonadThunkId m => MonadThunkId (ExceptT e m) where
+
+instance
+  MonadThunkId m
+  => MonadThunkId (ExceptT e m)
+ where
   type ThunkId (ExceptT e m) = ThunkId m
-instance MonadThunkId m => MonadThunkId (StateT s m) where
+
+instance
+  MonadThunkId m
+  => MonadThunkId (StateT s m)
+ where
   type ThunkId (StateT s m) = ThunkId m
 
-class MonadThunkId m => MonadThunk t m a | t -> m, t -> a where
+
+-- ** @class MonadThunk@
+
+class
+  MonadThunkId m
+  => MonadThunk t m a | t -> m, t -> a
+ where
 
   -- | Return an identifier for the thunk unless it is a pure value (i.e.,
   --   strictly an encapsulation of some 'a' without any additional
@@ -53,12 +84,20 @@ class MonadThunkId m => MonadThunk t m a | t -> m, t -> a where
   --   this modifies the thunk, for others it may create a new thunk.
   further  :: t -> m t
 
+
+-- ** @class MonadThunk@
+
 -- | Class of Kleisli functors for easiness of customized implementation developlemnt.
-class MonadThunkF t m a | t -> m, t -> a where
+class
+  MonadThunkF t m a | t -> m, t -> a
+ where
   queryMF   :: (a   -> m r) -> m r -> t -> m r
   forceF    :: (a   -> m r) -> t   -> m r
   forceEffF :: (a   -> m r) -> t   -> m r
   furtherF  :: (m a -> m a) -> t   -> m t
+
+
+-- ** @newtype ThunkLoop@
 
 newtype ThunkLoop = ThunkLoop Text -- contains rendering of ThunkId
   deriving Typeable
