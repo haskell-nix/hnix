cb1e23ebc542ef90cd97cd16bcbaf97a76c1993c
Anton-Latukha <anton.latukha@gmail.com>
2021-05-22 21:25:32 +0300

    Value.Monad: class MonadValue: add docs

--- src/Nix/Value/Monad.hs
+++ src/Nix/Value/Monad.hs
@@ -5,7 +5,9 @@ module Nix.Value.Monad where
 -- * @MonadValue@ - a main implementation class
 
 class MonadValue v m where
+  -- | Wrap value into a thunk.
   defer :: m v -> m v
+  -- | Force the evaluation of the value.
   demand :: v -> m v
   -- | If 'v' is a thunk, 'inform' allows us to modify the action to be
   --   performed by the thunk, perhaps by enriching it with scope info, for
