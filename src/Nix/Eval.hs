5b0d3fc6f7a7bd546aab5c088a81614a6480e583
Anton-Latukha <anton.latukha@gmail.com>
2021-11-08 00:45:11 +0200

    Eval: m clean-up

--- src/Nix/Eval.hs
+++ src/Nix/Eval.hs
@@ -466,8 +466,7 @@ evalSetterKeyName =
   \case
     StaticKey k -> pure $ pure k
     DynamicKey k ->
-      (coerce . ignoreContext) <<$>>
-        runAntiquoted "\n" assembleString (fromValueMay =<<) k
+      coerce . ignoreContext <<$>> runAntiquoted "\n" assembleString (fromValueMay =<<) k
 
 assembleString
   :: forall v m
