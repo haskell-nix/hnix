edf29bf23bf913d563fbb2f0908081b4370642ac
soulomoon <fwy996602672@gmail.com>
2022-01-05 14:56:18 +0000

    add builtins.hashfile

--- tests/NixLanguageTests.hs
+++ tests/NixLanguageTests.hs
@@ -56,8 +56,8 @@ groupBy key = Map.fromListWith (<>) . fmap (key &&& pure)
 -- previously passed.
 newFailingTests :: Set String
 newFailingTests = Set.fromList
-  [ "eval-okay-hash"
-  , "eval-okay-hashfile"
+  [ 
+    "eval-okay-hash"
   , "eval-okay-path"  -- #128
   , "eval-okay-types"
   , "eval-okay-fromTOML"
